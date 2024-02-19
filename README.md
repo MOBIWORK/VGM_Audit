bc8bc631aa17f9cd4aab259e07d3aa13c4a1af42
DuongXuanThang <xuanthang1909972@gmail.com>
2024-02-19 15:06:00 +0700

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## VGM-Audit
+
+VGM
+
+#### License
+
+MIT
---
+++ vgm_audit/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "VGM-Audit",
+			"type": "module",
+			"label": _("VGM-Audit")
+		}
+	]
