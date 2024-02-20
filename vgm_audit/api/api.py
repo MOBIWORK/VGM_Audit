1e64b62b8f6c3a733e41ad83bb33b552fa3ebde0
DuongXuanThang <xuanthang1909972@gmail.com>
2024-02-20 17:16:23 +0700

    fix check image product exist

--- vgm_audit/api/api.py
+++ vgm_audit/api/api.py
@@ -8,35 +8,47 @@ from deepvision import DeepVision
 from deepvision.service import ProductCountService
 @frappe.whitelist(methods=["POST"])
 # param {items: arr,doctype: ''}
-def deleteList(*args,**kwargs): 
+def deleteListByDoctype(*args,**kwargs): 
+    RECOGNITION_API_KEY: str = '00000000-0000-0000-0000-000000000002'
+    deep_vision: DeepVision = DeepVision()
+    product_recognition: ProductRecognitionService = deep_vision.init_product_recognition_service(RECOGNITION_API_KEY)
+    products: Products = product_recognition.get_products()
     try:
         # Chuyển đổi ids từ chuỗi JSON thành danh sách Python
         ids_list = frappe.parse_json(kwargs.get('items'))
 
         # Lặp qua danh sách ids và thực hiện xóa
         for id in ids_list:
-            # Thực hiện xóa cho mỗi id
-            frappe.delete_doc(kwargs.get('doctype'), id)
-
-        return {"status": "success", "message": "Deleted successfully"}
+            if kwargs.get('doctype') == "VGM_Product":
+                # Trong trường hợp doctype là VGM_Product, xóa sản phẩm trong mô hình AI
+                doc = frappe.get_doc(kwargs.get('doctype'), id)
+                product_name = doc.product_name
+                collection_name = doc.category
+                products.delete_product_by_name(collection_name, product_name)
+                frappe.delete_doc(kwargs.get('doctype'), id)
+            else:
+                frappe.delete_doc(kwargs.get('doctype'), id)
+        return {"status": "success"}
     except Exception as e:
-        return {"status": "error", "message": str(e)}
-@frappe.whitelist(methods=["POST"])
+        return {"status": "error"}
+@frappe.whitelist(methods=["POST"],allow_guest=True)
 # param {items: arr,doctype: ''}
 def checkImageProductExist(*args,**kwargs):
     RECOGNITION_API_KEY: str = '00000000-0000-0000-0000-000000000002'
     deep_vision: DeepVision = DeepVision()
     recognition: ProductCountService = deep_vision.init_product_count_service(RECOGNITION_API_KEY)
     base_url = frappe.utils.get_request_site_address()
-    collection_name = 'VGM_Product'
-    linkimages = frappe.parse_json(kwargs.get('linkimages'))
+    collection_name = kwargs.get('collection_name')
+    linkimages = kwargs.get('linkimages')
     
     image_path = base_url + linkimages
-    product_id = self.product
-    get_product_name =  frappe.get_value("Product", {"name": product_id}, "product_name")
+    # product_id = self.product
+    # get_product_name =  frappe.get_value("Product", {"name": product_id}, "product_name")
     response = recognition.count(collection_name, image_path)
     if response.get('status') == 'completed':
-        count_value = response.get('result', {}).get('count', {}).get(get_product_name)
-        self.set('sum', count_value)
+        count_value = response.get('result', {}).get('count', {})
+        return count_value
+        # self.set('sum', count_value)
     else:
-        self.set('sum', self.sum)
+        return {"status": "error"}
+        # self.set('sum', self.sum)
