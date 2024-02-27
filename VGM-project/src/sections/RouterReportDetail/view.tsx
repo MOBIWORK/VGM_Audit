82873b8a2ee09845763a1ba98c519d3f00834bca
DuongXuanThang <xuanthang1909972@gmail.com>
2024-02-27 12:20:49 +0700

    filter_report

--- VGM-project/src/sections/RouterReportDetail/view.tsx
+++ VGM-project/src/sections/RouterReportDetail/view.tsx
@@ -1,7 +1,7 @@
 
 import { LuUploadCloud } from "react-icons/lu";
 import { FormItemCustom, HeaderPage, TableCustom } from "../../components";
-import  {AxiosService} from '../../services/server';
+import  {AxiosService, AxiosServiceMBW} from '../../services/server';
 import { VscAdd } from "react-icons/vsc";
 import {
   DeleteOutlined,
@@ -63,8 +63,141 @@ const rowSelection = {
 };
 
 export default function ReportDetail() {
+  const [searchCampaign, setSearchCampaign] = useState('');
+  const [searchTime, setSearchTime] = useState(null); // Sử dụng state để lưu giá trị thời gian thực hiện
+  const [searchEmployee, setSearchEmployee] = useState('all'); // Mặc định là 'all'
+  const [filteredDataReport, setFilteredDataReport] =  useState<any[]>([]);
+
   const navigate = useNavigate();
   const [dataReport, setDataReport] = useState<any[]>([]);
+  const dataEmployee =  [
+    {
+        "designation": null,
+        "email": "hoanganh@gmail.com",
+        "cell_number": null,
+        "name": "HR-EMP-00014",
+        "date_of_birth": 1017939600,
+        "employee_name": "Hoàng Anh",
+        "image": null
+    },
+    {
+        "designation": null,
+        "email": "hapt@mbw.vn",
+        "cell_number": null,
+        "name": "HR-EMP-00013",
+        "date_of_birth": 639766800,
+        "employee_name": "Hà PT",
+        "image": null
+    },
+    {
+        "designation": null,
+        "email": "lamthatnhanh111@gmail.com",
+        "cell_number": null,
+        "name": "HR-EMP-00012",
+        "date_of_birth": 987181200,
+        "employee_name": "Vương Linh",
+        "image": null
+    },
+    {
+        "designation": null,
+        "email": "haudang130197@gmail.com",
+        "cell_number": null,
+        "name": "HR-EMP-00011",
+        "date_of_birth": 891709200,
+        "employee_name": "Đặng Hậu",
+        "image": "http://hr.mbwcloud.com:8007/private/files/growing-hydrangeas-1402684-01-3942-3246-1680492267.jpg"
+    },
+    {
+        "designation": null,
+        "email": "hong23n@gmail.com",
+        "cell_number": null,
+        "name": "HR-EMP-00010",
+        "date_of_birth": 891709200,
+        "employee_name": "Thúy Hồng",
+        "image": "http://hr.mbwcloud.com:8007/private/files/avtDefault.png"
+    },
+    {
+        "designation": null,
+        "email": "longkhuat0902@gmail.com",
+        "cell_number": null,
+        "name": "HR-EMP-00009",
+        "date_of_birth": 1017939600,
+        "employee_name": "Khuất Long",
+        "image": null
+    },
+    {
+        "designation": null,
+        "email": "chuquynhanh256@gmail.com",
+        "cell_number": null,
+        "name": "HR-EMP-00008",
+        "date_of_birth": 923158800,
+        "employee_name": "Chu Anh",
+        "image": "http://hr.mbwcloud.com:8004/files/avarta_HR-EMP-00005_2023-11-11 10:50:38.394703_b58c7d.png"
+    },
+    {
+        "designation": null,
+        "email": "ductuan1999@gmail.com",
+        "cell_number": null,
+        "name": "HR-EMP-00007",
+        "date_of_birth": 923763600,
+        "employee_name": "Đức Tuấn",
+        "image": null
+    },
+    {
+        "designation": null,
+        "email": "tungda@mobiwork.vn",
+        "cell_number": null,
+        "name": "HR-EMP-00006",
+        "date_of_birth": 607798800,
+        "employee_name": "Đỗ Tùng",
+        "image": "http://hr.mbwcloud.com:8007/private/files/IMG_20220327_1504415734df.jpg"
+    },
+    {
+        "designation": null,
+        "email": "phongtran100401@gmail.com",
+        "cell_number": null,
+        "name": "HR-EMP-00005",
+        "date_of_birth": 986835600,
+        "employee_name": "Trần Phong",
+        "image": null
+    },
+    {
+        "designation": null,
+        "email": null,
+        "cell_number": null,
+        "name": "HR-EMP-00004",
+        "date_of_birth": 1563123600,
+        "employee_name": "Nguyễn Thanh Mạnh",
+        "image": null
+    },
+    {
+        "designation": null,
+        "email": null,
+        "cell_number": null,
+        "name": "HR-EMP-00003",
+        "date_of_birth": 950893200,
+        "employee_name": "Hà Chi",
+        "image": null
+    },
+    {
+        "designation": null,
+        "email": "ngocsondds@gmail.com",
+        "cell_number": null,
+        "name": "HR-EMP-00002",
+        "date_of_birth": 959706000,
+        "employee_name": "Ngọc Sơn",
+        "image": null
+    },
+    {
+        "designation": null,
+        "email": "chuyendev@gmail.com",
+        "cell_number": null,
+        "name": "HR-EMP-00001",
+        "date_of_birth": 1694019600,
+        "employee_name": "Lê Công Chuyện",
+        "image": null
+    }
+]
   const [selectionType, setSelectionType] = useState<"checkbox" | "radio">(
     "checkbox"
   );
@@ -91,7 +224,10 @@ export default function ReportDetail() {
           }
         })
         setDataReport(dataReport);
+        setFilteredDataReport(dataReport);
       }
+
+
     } catch (error) {
     } finally {
     }
@@ -101,60 +237,55 @@ export default function ReportDetail() {
     fetchDataReport();
   }, []);
 
+  useEffect(() => {
+    // Lọc dữ liệu báo cáo dựa trên các trường tìm kiếm khi có sự thay đổi
+    const filteredData = dataReport.filter(record => {
+      const matchCampaign = record.campaign_name.toLowerCase().includes(searchCampaign.toLowerCase());
+      // Xử lý lọc theo thời gian nếu cần
+      // const matchTime = true; // Đặt điều kiện mặc định
+      // Xử lý lọc theo nhân viên
+      const matchTime = searchTime && searchTime[0] && searchTime[1] ?
+      new Date(record.date_check_in) >= searchTime[0] && new Date(record.date_check_in) <= searchTime[1] : true;
+      const matchEmployee = searchEmployee === 'all' || record.employee_code === searchEmployee;
+      return matchCampaign && matchEmployee && matchTime; // && matchTime nếu cần thêm điều kiện thời gian
+    });
+    setFilteredDataReport(filteredData);
+  }, [searchCampaign, searchTime, searchEmployee, dataReport]);
   return (
     <>
       <HeaderPage
         title="Báo cáo"
-        // buttons={[
-        //   {
-        //     label: "Xóa",
-        //     type: "primary",
-        //     icon: <DeleteOutlined />,
-        //     size: "20px",
-        //     className: "flex items-center mr-2",
-        //     danger: true,
-        //   },
-        //   {
-        //     label: "Nhập file",
-        //     icon: <LuUploadCloud className="text-xl" />,
-        //     size: "20px",
-        //     className: "flex items-center mr-2",
-        //   },
-        //   {
-        //     label: "Thêm mới",
-        //     type: "primary",
-        //     icon: <VscAdd className="text-xl" />,
-        //     size: "20px",
-        //     className: "flex items-center",
-        //     action: () => navigate('/campaign-create')
-        //   },
-        // ]}
       />
       <div className="bg-white rounded-xl">
         <div className="flex p-4" style={{ alignItems: 'flex-end' }}>
       <FormItemCustom className="w-[320px] border-none mr-4 " >
-        <Input placeholder="Tìm kiếm theo chiến dịch" prefix={<SearchOutlined />} />
+        <Input placeholder="Tìm kiếm theo chiến dịch" 
+         value={searchCampaign}
+         onChange={(e) => setSearchCampaign(e.target.value)}
+         prefix={<SearchOutlined />} />
       </FormItemCustom>
       <FormItemCustom className="w-[250px] border-none mr-4" label="Thời gian thực hiện">
-        <RangePicker />
+        <RangePicker  value={searchTime}
+             onChange={(dates) => setSearchTime(dates)}/>
       </FormItemCustom>
-    
   <div style={{ display: 'flex', flexDirection: 'column' }}>
-    <label style={{paddingBottom: '5px'}}>Nhân viên:</label>
-    <Select className="w-[200px] h-[36px]">
-      <Select.Option value="demo">Nguyễn Văn A</Select.Option>
-      <Select.Option value="demo1">Nguyễn Văn B</Select.Option>
-      <Select.Option value="demo2">Nguyễn Văn C</Select.Option>
-      <Select.Option value="demo3">Nguyễn Văn D</Select.Option>
-
-    </Select>
-  </div>
+  <label style={{paddingBottom: '5px'}}>Nhân viên:</label>
+  <Select className="w-[200px] h-[36px]" value={searchEmployee}
+            onChange={(value) => setSearchEmployee(value)} defaultValue="all">
+    <Select.Option value="all">Tất cả</Select.Option>
+    {dataEmployee.map(employee => (
+      <Select.Option key={employee.name} value={employee.name}>
+        {employee.employee_name}
+      </Select.Option>
+    ))}
+  </Select>
+</div>
 
     </div>
         <div className="p-4">
           <TableCustom
             columns={columns}
-            dataSource={dataReport}
+            dataSource={filteredDataReport}
             onRow={(record, rowIndex) => {
               return {
                 onClick: () => handleRowClick(record), // Gọi hàm xử lý khi click vào dòng
