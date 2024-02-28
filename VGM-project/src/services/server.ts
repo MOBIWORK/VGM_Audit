875825dceda64b2ff16aca381e9fce83fa90fdce
Duy.eKGIS <77255059+dev3ekgis@users.noreply.github.com>
2024-02-28 13:33:17 +0700

    Update server.ts

--- VGM-project/src/services/server.ts
+++ VGM-project/src/services/server.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 let AxiosService:any = axios;
-let AxiosServiceMBW:any = axios;
+// console.log(import.meta.env.VITE_BASE_URL);
 
 if (import.meta.env.VITE_BASE_URL) {
+  console.log('vao day');
+  
   AxiosService = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
   });
@@ -21,31 +23,12 @@ if (import.meta.env.VITE_BASE_URL) {
   );
   
 }
-if(import.meta.env.VITE_MBW_PATH){
-  AxiosServiceMBW = axios.create({
-    baseURL: import.meta.env.VITE_MBW_PATH,
-  });
-  AxiosServiceMBW.interceptors.request.use(
-    function (config) {
-      const authString = `Basic ${btoa(`${import.meta.env.VITE_AUTH_MBW_KEY}:${import.meta.env.VITE_AUTH_MBW_SECRET}`)}`
-      config['headers']['Authorization'] =authString
-      // Do something before request is sent
-      return config;
-    },
-    function (error) {
-      // Do something with request error
-      return Promise.reject(error);
-    }
-  );
-}
 AxiosService.interceptors.response.use(
   function (response) {
-    // console.log("response",response);
+    console.log("response",response);
     
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    console.log(response, response.data);
-    console.log(typeof(response));
     return response.data;
   },
   function (error) {
@@ -54,13 +37,5 @@ AxiosService.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-AxiosServiceMBW.interceptors.response.use(
-  function (response){
-    return response.result;
-  },
-  function (error){
-    return Promise.reject(error);
-  }
-);
 
-export { AxiosService, AxiosServiceMBW };
+export { AxiosService };
