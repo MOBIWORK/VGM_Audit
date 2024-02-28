b064f00840b1ee4a9e8226e1f98927d3cd86bf4e
aedudon <109790101+aedudon@users.noreply.github.com>
2024-02-28 09:24:38 +0700

    Update server.ts

--- VGM-project/src/services/server.ts
+++ VGM-project/src/services/server.ts
@@ -45,7 +45,7 @@ AxiosService.interceptors.response.use(
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     console.log(response);
-    return response;
+    return response.data;
   },
   function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
